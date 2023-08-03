d14e899fd63674bbf19f9a5aad5d9ba9182dbc21
mukhtarali212 <mukhtarali212@gmail.com>
2023-08-03 18:31:43 +0530

    terraform file

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = var.aws_region
+  
+}
+
+resource "aws_security_group" "my-security-group" {
+    name        = var.security_group
+    description = "ec2 instance security group"
+
+    ingress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = var.security_group
+    }
+}
+
+resource "aws_instance" "test_instance" {
+    ami             = var.ami_id
+    key_name        = var.key_name
+    instance_type   = var.instance_type
+    security_group  = [var.security_group]
+    tags {
+        Name    = var.tag_name
+    }
+}
---
+++ variable.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+    default     = "ap-south-1" 
+}
+
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "Centos" 
+}
+
+variable "instance_type" { 
+    default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+    default     = "test-sgroup-" 
+}
+
+variable "tag_name" { 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    default     = "ami-0f5ee92e2d63afc18" 
+}
+variable "versioning" {
+    type        = bool
+    default     = true
+}
+variable "acl" {
+    type        = string
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    default     = "my-s3bucket-"
+}
+variable "tags" {
+    type        = map
+    default     = {
+    environment = "DEV"
+    terraform   = "true"
+    }
+}
